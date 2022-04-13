f8f3f87c2a6c0306c29536add30b7a3fbb61abba
Clarence Choi <32627808+riderchoi@users.noreply.github.com>
2022-04-13 15:47:46 -0400

    Add files via upload

--- wfmcsv.py
+++ wfmcsv.py
@@ -1,6 +1,7 @@
 import csv
 from datetime import timezone, date, timedelta
 import datetime
+import random
 
 def daterange(start_date,end_date):
   for n in range(int((end_date - start_date).days)):
@@ -18,6 +19,9 @@ end_year = input("Enter the end year:")
 end_month = input("Enter the end month:")
 end_day = input("Enter the end day:")
 end_date = date(int(end_year),int(end_month),int(end_day))
+incoming_contact_volume = input("Enter incoming contact volume including abandoned call, plus or minus 5 to 10 contacts:")
+avg_handle_time = input("Average Handle Time, will add or minus up to 20 seconds:")
+# contacts_handled = input("Contacts that are handled, minus 3 to 5 contacts:")
 
 
 row_list = [
@@ -31,8 +35,29 @@ row_list = [
  "ContactsHandled"]]
 
 for single_date in daterange(start_date, end_date):
-  row_list.insert(len(row_list), [queue_name,queue_id,channel_type,single_date.strftime("%Y-%m-%dT00:00:00Z"),interval_value,"50","27","645"])
+  icv = random.randrange(int(incoming_contact_volume)-5,int(incoming_contact_volume)+5)
+  aht = random.randrange( int(avg_handle_time)-20 , int(avg_handle_time)+20 )
+  ch = icv - random.randrange(3,5)
+  if interval_value == 'daily':
+    row_list.insert(len(row_list), [queue_name,queue_id,channel_type,single_date.strftime("%Y-%m-%dT00:00:00Z"),interval_value,icv,aht,ch])
+  elif interval_value == '15mins':
+    for hours in range (23):
+      for minutes in range(0,60,15):
+        icv = random.randrange(int(incoming_contact_volume)-5,int(incoming_contact_volume)+5)
+        aht = random.randrange( int(avg_handle_time)-20 , int(avg_handle_time)+20 )
+        ch = icv - random.randrange(3,5)
+        timestamp = single_date.strftime("%Y-%m-%dT")+f"{hours:02}"+":"+f"{minutes:02}"+":"+"00Z"
+        row_list.insert(len(row_list), [queue_name,queue_id,channel_type,timestamp,interval_value,icv,aht,ch])
+  elif interval_value == '30mins':
+    for hours in range (23):
+      for minutes in range(0,60,30):
+        icv = random.randrange(int(incoming_contact_volume)-5,int(incoming_contact_volume)+5)
+        aht = random.randrange( int(avg_handle_time)-20 , int(avg_handle_time)+20 )
+        ch = icv - random.randrange(3,5)
+        timestamp = single_date.strftime("%Y-%m-%dT")+f"{hours:02}"+":"+f"{minutes:02}"+":"+"00Z"
+        row_list.insert(len(row_list), [queue_name,queue_id,channel_type,timestamp,interval_value,icv,aht,ch])
+	
 
-with open ('test.csv','w',newline='') as file:
+with open (queue_name+'-'+interval_value+'.csv','w',newline='') as file:
   writer = csv.writer(file)
   writer.writerows(row_list)
